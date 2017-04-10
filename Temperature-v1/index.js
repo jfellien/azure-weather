bd7c6ba0b3ec892fbe845a478eb2007cf9cb780d
Jan Fellien <jan.fellien@devcrowd.de>
2017-04-10 22:40:40 +0200

    added temperature function

---
+++ Temperature-v1/index.js
@@ -0,0 +1,21 @@
+module.exports = (context, req) => {
+
+
+    context.log('JavaScript HTTP trigger function processed a request.');
+    
+
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            body: "Hello " + (req.query.name || req.body.name)
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+    context.done();
+
+    
+};
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Schummelzettel
+
+## Create a function
+
+`func function create`
